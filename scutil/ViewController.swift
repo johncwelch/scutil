7c1c5a6058aba55dabdb2c5824f23a4df73e14a8
John C. Welch <jwelch@bynkii.com>
2022-02-11 12:54:20 -0600

    Initial Commit

---
+++ scutil/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  scutil
+//
+//  Created by John Welch on 2/11/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
